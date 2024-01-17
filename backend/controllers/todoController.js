95cc9b9dea0d8e9e95d3362ae6913f69d699a0d5
Anastasia <ahmadhamsa01@gmail.com>
2024-01-18 00:17:45 +0800

    add create DB, table & list API

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -8,3 +8,25 @@ exports.createDB = (req, res) => {
         return res.status(201).json("DB Created");
     });
 }
+
+
+// CREATE TABEL
+exports.createTable = (req, res) => {
+    let q = 'CREATE TABLE todos(id int AUTO_INCREMENT, firstname VARCHAR(255), lastname VARCHAR(255), PRIMARY KEY(id))';
+    db.query(q, (err, result) => {
+        if (err) throw err;
+        return res.status(201).json("TABLE CREATED")
+    });
+}
+
+//CREATE LIST
+exports.createList = (req, res) => {
+    const q = "INSERT INTO todos SET ?";
+
+    const { firstName, lastName } = req.body;
+
+    db.query(q, {firstName, lastName}, (err, result) => {
+        if (err) return res.json(err);
+        return res.status(200).json(result);
+    });
+}
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createDB } = require('../controllers/todoController');
+const { createDB, createTable, createList } = require('../controllers/todoController');
 const router = express.Router();
 
 //ROUTES
-router.get('/create/database', createDB)
+router.get('/create/database', createDB);
+router.get('/create/table', createTable);
+router.get('/create/list', createList);
 
 module.exports = router;
