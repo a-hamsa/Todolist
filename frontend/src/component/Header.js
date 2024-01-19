07905fb49b8527d1712fd3b7117530aeac37558e
Anastasia <ahmadhamsa01@gmail.com>
2024-01-20 00:10:34 +0800

    frontend start

---
+++ frontend/src/component/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <div className='container-fluid' style={{background: "blue", padding: "15px 0", color: "#fafafa" }}>
+                <h5 style={{color: "#fafafa"}}>TODO LIST - REACT-NODEJS-MYSQL</h5>
+            </div>
+        </>
+    )
+}
+
+export default Header
